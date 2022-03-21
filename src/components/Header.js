71dbb9c9389b1ec53db64f145017454c41944e85
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-03-21 10:55:28 +0300

    edit next config

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,22 @@
+import Image from "next/image";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      {/* top nav */}
+      <div>
+        <div>
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+          />
+        </div>
+      </div>
+      {/* bottom nav */}
+      <div></div>
+    </header>
+  );
 }
 
 export default Header;
