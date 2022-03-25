6486f6bb5b0e35816b190989edfa747c13303c03
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-03-25 15:17:12 +0300

    implement firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA76-Gka4lRB-lO3jaIQCCbgoHhJOnGc5Q",
+  authDomain: "clone-1042a.firebaseapp.com",
+  projectId: "clone-1042a",
+  storageBucket: "clone-1042a.appspot.com",
+  messagingSenderId: "5090561209",
+  appId: "1:5090561209:web:4dab39aae82eebd10a9f68",
+};
