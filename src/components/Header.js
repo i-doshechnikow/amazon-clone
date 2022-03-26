fccf555d1bfc435993bde1863e30f50b3448fba1
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-03-26 13:28:18 +0300

    add signin methhod

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,8 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 
+import { useSession, signIn, signOut } from "next-auth/react";
+
 function Header() {
   return (
     <header>
@@ -28,7 +30,7 @@ function Header() {
         </div>
 
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div className="link">
+          <div onClick={signIn} className="link">
             <p>hello director</p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
