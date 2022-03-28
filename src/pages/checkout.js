1bbaa19a10536cd3987c5f7b1c196e81e874bb85
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-03-28 12:29:57 +0300

    add header to checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,5 +1,11 @@
+import Header from "../components/Header";
+
 function checkout() {
-  return <div>checkout</div>;
+  return <div className="bg-gray-100">
+    <Header />
+
+    <h1>CHECKOUT PAGE</h1>
+  </div>;
 }
 
 export default checkout;
