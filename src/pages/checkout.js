7b275407b3079a104b587e796b9d831b5f64ca3b
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-03-29 10:45:31 +0300

    build left side on checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Header from "../components/Header";
 
 function checkout() {
@@ -5,7 +6,22 @@ function checkout() {
     <Header />
 
     <main className="lg:flex max-w-screen-xl mx-auto">
-      <h1>test</h1>
+      {/* left */}
+      <div className="flex-grow m-5 shadow-sm">
+        <Image
+          src='https://links.papareact.com/ikj'
+          width={1020}
+          height={250}
+          objectFit='contain'
+        />
+
+        <div className="flex flex-col p-5 space-y-10 bg-white">
+          <h1 className="text-3xl border-b pb-4">Your Shopping Basket Is Empty</h1>
+        </div>
+      </div>
+
+      {/* right */}
+      <div></div>
     </main>
   </div>;
 }
